29fa9d9ee152e0b42cd52be720be38bf1ba0ed32
David Liu <77701856+daichungus@users.noreply.github.com>
2024-07-22 16:54:08 -0700

    Add files via upload

---
+++ patchy_chungus/repo.js
@@ -0,0 +1,11 @@
+{
+    "id": "daichungus",
+    "title": "Repo of Daichungus",
+    "contact": "shitpostman14@gmail.com",
+    "servers": [
+        ""
+    ],
+    "patches": {
+        "patchy_chungus": "(TH06) Replaces Patchouli with Big Chungus"
+    }
+}
