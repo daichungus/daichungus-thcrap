ebc29e7cec6996ab07d34a9ab81d030769cfc8f5
David Liu <77701856+daichungus@users.noreply.github.com>
2024-07-22 16:49:35 -0700

    Add files via upload

---
+++ patchy_chungus/files.js
@@ -0,0 +1,10 @@
+{
+  "th06/face08a.png": 2673889783,
+  "th06/face08a_a.png": null,
+  "th06/face08b.png": 2280689082,
+  "th06/face08b_a.png": null,
+  "th06/stg4enm.png": 1844759364,
+  "th06/stg4enm_a.png": 2726053243,
+  "th06/stg5enm.png": 2134790192,
+  "th06/stg5enm_a.png": 2726053243
+}
---
+++ patchy_chungus/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "patchy_chungus",
+	"servers": [
+		""
+	],
+	"title": "(TH06) Replaces Patchouli with Big Chungus",
+	"update": true
+}
