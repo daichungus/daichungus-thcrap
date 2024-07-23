865bdcc4b125b2310111a7ee0e542977fd9123c3
David Liu <77701856+daichungus@users.noreply.github.com>
2024-07-22 17:43:05 -0700

    Update patch.js

--- patchy_chungus/patch.js
+++ patchy_chungus/patch.js
@@ -4,7 +4,7 @@
 	],
 	"id": "patchy_chungus",
 	"servers": [
-		"https://mirrors.thpatch.net/daichungus/patchy_chungus"
+		"https://mirrors.thpatch.net/Daichungus/patchy_chungus"
 	],
 	"title": "(TH06) Replaces Patchouli with Big Chungus",
 }
