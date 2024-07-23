f170d9ffb211e866d7c407d495d8e44bbb18686b
David Liu <77701856+daichungus@users.noreply.github.com>
2024-07-22 17:03:44 -0700

    Update patchy_chungus.js

--- patchy_chungus/patchy_chungus.js
+++ patchy_chungus/patchy_chungus.js
@@ -6,6 +6,12 @@
       "archive": "repos/nmlgc/base_tsa/"
     },
     {
+      "archive: "repos/thpatch/script_latin/"
+    },
+    {
+      "archive: "repos/thpatch/western_name_order/"
+    },
+    {
       "archive": "repos/daichungus/patchy_chungus"
     }
   ]
