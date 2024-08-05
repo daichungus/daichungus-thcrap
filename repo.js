f8c0bede1ef23b30360e067a4ad922ccdfa55770
David Liu <77701856+daichungus@users.noreply.github.com>
2024-08-05 16:04:50 -0700

    updated format of repo.js
    i didn't do it, it just changed like that after I ran repo_update.js

--- repo.js
+++ repo.js
@@ -1,13 +1,13 @@
 {
-    "id": "daichungus",
-    "title": "Repo of Daichungus",
-    "contact": "Discord daichungus",
-    "servers": [
-        "https://mirrors.thpatch.net/Daichungus/",
-	"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/"
-    ],
-    "patches": {
-        "patchy_chungus": "(TH06) Replace Patchouli with Big Chungus",
-        "mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]"
-    }
+	"contact": "Discord daichungus",
+	"id": "daichungus",
+	"patches": {
+		"mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]",
+		"patchy_chungus": "(TH06) Replace Patchouli with Big Chungus"
+	},
+	"servers": [
+		"https://mirrors.thpatch.net/Daichungus/",
+		"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/"
+	],
+	"title": "Repo of Daichungus"
 }
