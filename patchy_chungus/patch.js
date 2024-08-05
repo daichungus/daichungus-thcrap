870d58efacaa2ae9f8540bc368d1a91b2ba86f17
David Liu <77701856+daichungus@users.noreply.github.com>
2024-08-05 14:34:29 -0700

    added new server link

--- patchy_chungus/patch.js
+++ patchy_chungus/patch.js
@@ -4,7 +4,8 @@
 	],
 	"id": "patchy_chungus",
 	"servers": [
-		"https://mirrors.thpatch.net/Daichungus/patchy_chungus"
+		"https://mirrors.thpatch.net/Daichungus/patchy_chungus",
+		"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/patchy_chungus"
 	],
 	"title": "(TH06) Replaces Patchouli with Big Chungus",
 }
