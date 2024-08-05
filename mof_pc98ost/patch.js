646dbc57cd8fc8f47548d7657043ec950d19f405
David Liu <77701856+daichungus@users.noreply.github.com>
2024-08-05 14:33:28 -0700

    added new server link

--- mof_pc98ost/patch.js
+++ mof_pc98ost/patch.js
@@ -4,7 +4,8 @@
 	],
 	"id": "mof_pc98ost",
 	"servers": [
-		"https://mirrors.thpatch.net/Daichungus/mof_pc98ost"
+		"https://mirrors.thpatch.net/Daichungus/mof_pc98ost",
+		"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/mof_pc98ost"
 	],
 	"title": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]",
 }	
