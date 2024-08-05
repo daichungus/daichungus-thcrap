4758dbfbc84802dc2c48eebbc59f092286bbaf6f
David Liu <77701856+daichungus@users.noreply.github.com>
2024-08-05 09:25:13 -0700

    fixed repo.js

--- repo.js
+++ repo.js
@@ -6,7 +6,7 @@
         "https://mirrors.thpatch.net/Daichungus/"
     ],
     "patches": {
-        "patchy_chungus": "(TH06) Replace Patchouli with Big Chungus"
-	"mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]"
+        "patchy_chungus": "(TH06) Replace Patchouli with Big Chungus",
+        "mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]"
     }
 }
