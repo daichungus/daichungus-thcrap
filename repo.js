33e7a01895b74f76ad5e74e123ccd2ce956fb7c9
David Liu <77701856+daichungus@users.noreply.github.com>
2024-08-05 14:33:03 -0700

    added new server link

--- repo.js
+++ repo.js
@@ -3,7 +3,8 @@
     "title": "Repo of Daichungus",
     "contact": "Discord daichungus",
     "servers": [
-        "https://mirrors.thpatch.net/Daichungus/"
+        "https://mirrors.thpatch.net/Daichungus/",
+	"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/"
     ],
     "patches": {
         "patchy_chungus": "(TH06) Replace Patchouli with Big Chungus",
