18575ba16227787a8e3c5cb40c77accf6dfadfbe
David Liu <77701856+daichungus@users.noreply.github.com>
2024-09-01 17:46:18 -0400

    added month_first patch

--- repo.js
+++ repo.js
@@ -3,7 +3,8 @@
 	"id": "daichungus",
 	"patches": {
 		"mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]",
-		"patchy_chungus": "(TH06) Replace Patchouli with Big Chungus"
+		"patchy_chungus": "(TH06) Replace Patchouli with Big Chungus",
+		"month_first": "Replace various characters' themes with Philiponbread's MONTH FIRST remixes"
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/Daichungus/",
