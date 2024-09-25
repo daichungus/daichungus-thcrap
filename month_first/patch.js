195eccda9c3d7de4eae1e3f51a7458ae5e3a8a84
David Liu <77701856+daichungus@users.noreply.github.com>
2024-09-25 19:43:55 -0400

    updated patch.js

--- month_first/patch.js
+++ month_first/patch.js
@@ -1,11 +1,11 @@
 {
-	"dependencies": [
-		"nmlgc/base_tsa"
-	],
-	"id": "month_first",
-	"servers": [
-		"https://mirrors.thpatch.net/Daichungus/month_first",
-		"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/month_first"
-	],
-	"title": "Replace various characters' themes with Philiponbread's MONTH FIRST remixes",
-}	
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "month_first",
+  "servers": [
+    "https://mirrors.thpatch.net/Daichungus/month_first/",
+    "https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/month_first/"
+  ],
+  "title": "(TH09, 10) Replace Cirno and Minoriko's theme with Philiponbread's remixes"
+}
