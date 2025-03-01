416d9caf63126c0cfaf98851e59373a7bc8af8b2
David Liu <liu.david.minecraft@gmail.com>
2025-03-01 12:32:40 -0500

    updated patch.js

--- month_first/patch.js
+++ month_first/patch.js
@@ -7,5 +7,5 @@
     "https://mirrors.thpatch.net/Daichungus/month_first/",
     "https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/month_first/"
   ],
-  "title": "(TH09, 10) Replace Cirno and Minoriko's theme with Philiponbread's remixes"
+  "title": "(TH09, 10) Replace various characters' theme with Philiponbread's remixes"
 }
