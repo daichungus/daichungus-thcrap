de59fa6ac8eeda5d3ce0b83b14e6ec99a2ccb1dc
David Liu <77701856+daichungus@users.noreply.github.com>
2025-03-01 12:36:14 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,14 +1,14 @@
 {
-	"contact": "Discord daichungus",
-	"id": "daichungus",
-	"patches": {
-		"mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]",
-		"patchy_chungus": "(TH06) Replace Patchouli with Big Chungus",
-		"month_first": "Replace various characters' themes with Philiponbread's MONTH FIRST remixes"
-	},
-	"servers": [
-		"https://mirrors.thpatch.net/Daichungus/",
-		"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/"
-	],
-	"title": "Repo of Daichungus"
+    "id": "daichungus",
+    "title": "Repo of Daichungus",
+    "contact": "Discord daichungus",
+    "servers": [
+        "https://mirrors.thpatch.net/Daichungus/",
+        "https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/"
+    ],
+    "patches": {
+        "mof_pc98ost": "(TH10) Replace OST with NotQuiteHere's PC-98 Arranges [Akyuu's Classic Score Volume 1]",
+        "month_first": "Replace various characters' themes with Philiponbread's MONTH FIRST remixes",
+        "patchy_chungus": "(TH06) Replace Patchouli with Big Chungus"
+    }
 }
