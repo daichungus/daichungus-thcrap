c5e9d06c063b6613e147106d82ef701598745a5e
David Liu <77701856+daichungus@users.noreply.github.com>
2025-03-21 22:11:49 -0400

    Begin never start patch

---
+++ never_starts/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "never_starts",
+  "servers": [
+    "https://mirrors.thpatch.net/Daichungus/never_starts/",
+    "https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/never_starts/"
+  ],
+  "title": "[WIP] (TH06, 07, 10) Touhou themes but they never start"
+}
