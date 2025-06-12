b02d16e28c496f62711fd092ea640a3f610a4d49
David Liu <77701856+daichungus@users.noreply.github.com.>
2025-06-12 00:17:55 -0700

    Added never starts to repo list

--- repo.js
+++ repo.js
@@ -9,6 +9,7 @@
     "patches": {
         "patchy_chungus": "(TH06) Replace Patchouli with Big Chungus",
         "mof_pc98ost": "(TH10) Replace OST with a PC-98 arrange album (by NotQuiteHere)",
-        "month_first": "Replace select tracks with Philiponbread's MONTH FIRST remixes"
+        "month_first": "Replace select tracks with Philiponbread's MONTH FIRST remixes",
+        "never_starts": "(TH06, 07) Regular OST but every track will get stuck in a limbo"
     }
 }
