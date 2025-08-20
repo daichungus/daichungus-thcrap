002a759b09a557dd1a6bc639aa26731574b1c76e
77701856+daichungus@users.noreply.github.com <77701856+daichungus@users.noreply.github.com>
2025-08-19 19:04:13 -0700

    Changes by THCRAP configuration

--- pcb_goty/patch.js
+++ pcb_goty/patch.js
@@ -1,11 +1,11 @@
 {
-	"dependencies": [
-		"nmlgc/base_tsa"
-	],
-	"id": "pcb_goty",
-	"servers": [
-		"https://mirrors.thpatch.net/Daichungus/pcb_goty",
-		"https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/pcb_goty"
-	],
-	"title": "(TH07) Paint Cherry Blossom by Gastari et al.",
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "pcb_goty",
+  "servers": [
+    "https://mirrors.thpatch.net/Daichungus/pcb_goty/",
+    "https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/pcb_goty/"
+  ],
+  "title": "(TH07) Paint Cherry Blossom by Gastari et al."
 }
--- repo.js
+++ repo.js
@@ -7,10 +7,10 @@
         "https://raw.githubusercontent.com/daichungus/daichungus-thcrap/master/"
     ],
     "patches": {
-        "pcb_goty": "(TH07) Paint Cherry Blossom by Gastari et al.",
         "mof_pc98ost": "(TH10) Replace OST with a PC-98 arrange album (by NotQuiteHere)",
         "month_first": "Replace various characters' themes with Philiponbread's MONTH FIRST remixes",
         "never_starts": "(TH06, 07) Regular OST but every track will get stuck in a limbo",
-        "patchy_chungus": "(TH06) Replaces Patchouli with Big Chungus"
+        "patchy_chungus": "(TH06) Replaces Patchouli with Big Chungus",
+        "pcb_goty": "(TH07) Paint Cherry Blossom by Gastari et al."
     }
 }
